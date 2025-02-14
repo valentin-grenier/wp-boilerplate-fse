9c32a675f6cfe588a7c2bf5e3629a6a684626653
Valentin Grenier <65737131+valentin-grenier@users.noreply.github.com>
2025-02-14 11:50:27 +0100

    Changed single array arg to string

--- assets/js/unregister-style-variations.js
+++ assets/js/unregister-style-variations.js
@@ -1,10 +1,10 @@
 wp.domReady(function () {
-	wp.blocks.unregisterBlockStyle('core/button', ['outline', 'fill']);
-	wp.blocks.unregisterBlockStyle('core/image', ['rounded']);
-	wp.blocks.unregisterBlockStyle('core/quote', ['plain']);
-	wp.blocks.unregisterBlockStyle('core/site-logo', ['rounded']);
-	wp.blocks.unregisterBlockStyle('core/separator', ['wide', 'dots']);
-	wp.blocks.unregisterBlockStyle('core/social-links', ['pill-shape']);
-	wp.blocks.unregisterBlockStyle('core/table', ['stripes']);
-	wp.blocks.unregisterBlockStyle('core/tag-cloud', ['outline']);
+	wp.blocks.unregisterBlockStyle('core/button', ['fill', 'outline']);
+	wp.blocks.unregisterBlockStyle('core/image', 'rounded');
+	wp.blocks.unregisterBlockStyle('core/quote', 'plain');
+	wp.blocks.unregisterBlockStyle('core/site-logo', 'rounded');
+	wp.blocks.unregisterBlockStyle('core/social-links', ['logo-only', 'pill-shape']);
+	wp.blocks.unregisterBlockStyle('core/separator', 'dots');
+	wp.blocks.unregisterBlockStyle('core/table', 'stripes');
+	wp.blocks.unregisterBlockStyle('core/tag-cloud', 'outline');
 });
