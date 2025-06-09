d51f04432737188e29179cbdf5555bfbd42bee7e
Valentin <valentin.grenier02@gmail.com>
2025-06-09 12:44:17 +0200

    core: Edited comment format

--- wp-content/themes/theme-fse/functions.php
+++ wp-content/themes/theme-fse/functions.php
@@ -1,6 +1,10 @@
 <?php
 
-// Include all files from the inc directory
+/**
+ * Include all files withinh the inc directory
+ * 
+ * @return void
+ */
 foreach (glob(get_template_directory() . '/inc/*.php') as $file) {
     require_once $file;
 }
