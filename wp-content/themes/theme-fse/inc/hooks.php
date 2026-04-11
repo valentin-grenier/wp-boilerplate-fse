07096c0550ec48e219521628b4a85eaab30625cb
Valentin Grenier <65737131+valentin-grenier@users.noreply.github.com>
2026-04-11 22:39:01 +0200

    chore: Added hooks inc file with UpdraftPlus directory exclusion

---
+++ wp-content/themes/theme-fse/inc/hooks.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+/**
+ * Exclude node_modules directories from UpdraftPlus backups.
+ */
+function studio_exclude_node_modules_from_backup(bool $exclude, string $dir): bool
+{
+    if (str_contains($dir, 'node_modules') || str_contains($dir, 'vendor')) {
+        return true;
+    }
+    return $exclude;
+}
+add_filter('updraftplus_exclude_directory', 'studio_exclude_node_modules_from_backup', 10, 2);
