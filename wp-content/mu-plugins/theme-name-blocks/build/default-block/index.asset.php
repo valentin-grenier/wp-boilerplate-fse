903c50285ba1c28d2c8d8e53c0e1a771463e65ef
Valentin Grenier <valentin@studio-val.fr>
2026-04-15 14:46:00 +0200

    feat: Removed mu-plugins from repository

--- wp-content/mu-plugins/theme-name-blocks/build/default-block/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'afdb75bc550147fa89f1');
--- wp-content/mu-plugins/theme-name-blocks/src/default-block/view.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable no-console */
-console.log("Hello World! (from create-block-studio-blocks block)");
-/* eslint-enable no-console */
