9dd4b70feae8ec91496eb5725a634fb721ceb593
Valentin Grenier <valentin.grenier02@gmail.com>
2026-04-29 22:55:58 +0200

    Revert "Merge pull request #91 from valentin-grenier/feature/dashboard-widget"
    This reverts commit 75f383e8cc79bda149c3cc3f7a749eb386303232, reversing
    changes made to 5fe024dc32dcc525a39a9702c60a711c0082ee60.

--- wp-content/themes/theme-fse/dist/js/editor.bundle.js
+++ wp-content/themes/theme-fse/dist/js/editor.bundle.js
@@ -1,1 +1,142 @@
-(()=>{var e={892(){wp.domReady(function(){wp.blocks.unregisterBlockStyle("core/button",["fill","outline"]),wp.blocks.unregisterBlockStyle("core/image","rounded"),wp.blocks.unregisterBlockStyle("core/quote","plain"),wp.blocks.unregisterBlockStyle("core/site-logo","rounded"),wp.blocks.unregisterBlockStyle("core/social-links",["logo-only","pill-shape"]),wp.blocks.unregisterBlockStyle("core/separator","dots"),wp.blocks.unregisterBlockStyle("core/table","stripes"),wp.blocks.unregisterBlockStyle("core/tag-cloud","outline")})}},o={};function r(l){var t=o[l];if(void 0!==t)return t.exports;var s=o[l]={exports:{}};return e[l](s,s.exports,r),s.exports}(()=>{"use strict";r(892)})()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/editor/_unregister-style-variations.js"
+/*!***************************************************!*\
+  !*** ./js/editor/_unregister-style-variations.js ***!
+  \***************************************************/
+() {
+
+/**
+ * Unregister default block style variations.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-styles/#unregistering-block-styles
+ */
+wp.domReady(function () {
+  wp.blocks.unregisterBlockStyle('core/button', ['fill', 'outline']);
+  wp.blocks.unregisterBlockStyle('core/image', 'rounded');
+  wp.blocks.unregisterBlockStyle('core/quote', 'plain');
+  wp.blocks.unregisterBlockStyle('core/site-logo', 'rounded');
+  wp.blocks.unregisterBlockStyle('core/social-links', ['logo-only', 'pill-shape']);
+  wp.blocks.unregisterBlockStyle('core/separator', 'dots');
+  wp.blocks.unregisterBlockStyle('core/table', 'stripes');
+  wp.blocks.unregisterBlockStyle('core/tag-cloud', 'outline');
+});
+
+/***/ },
+
+/***/ "./js/editor/index.js"
+/*!****************************!*\
+  !*** ./js/editor/index.js ***!
+  \****************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _unregister_style_variations__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./_unregister-style-variations */ "./js/editor/_unregister-style-variations.js");
+/* harmony import */ var _unregister_style_variations__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_unregister_style_variations__WEBPACK_IMPORTED_MODULE_0__);
+
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		if (!(moduleId in __webpack_modules__)) {
+/******/ 			delete __webpack_module_cache__[moduleId];
+/******/ 			var e = new Error("Cannot find module '" + moduleId + "'");
+/******/ 			e.code = 'MODULE_NOT_FOUND';
+/******/ 			throw e;
+/******/ 		}
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it needs to be in strict mode.
+(() => {
+"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./js/editor.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _editor_index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./editor/index */ "./js/editor/index.js");
+
+})();
+
+// This entry needs to be wrapped in an IIFE because it needs to be in strict mode.
+(() => {
+"use strict";
+/*!**************************!*\
+  !*** ./scss/editor.scss ***!
+  \**************************/
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=editor.bundle.js.map
